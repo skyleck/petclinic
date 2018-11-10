0c55681f0dbdbed8707b67357f97af03174af0be
unknown <maximehempte@gmail.com>
2018-11-10 20:32:51 +0100

    Dockerfile petclinic-mysql and vet's sql-scripts

---
+++ Image-Mysql/sql-scripts/Vet_Insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO vet (lastname, firstname, speciality)
+VALUES ("Carter", "James", "none");
+
+INSERT INTO vet (lastname, firstname, speciality)
+VALUES ("Helen", "Leary", "radiology");
+
+INSERT INTO vet (lastname, firstname, speciality)
+VALUES ("Douglas", "Linda", "dentistry surgery");
+
+INSERT INTO vet (lastname, firstname, speciality)
+VALUES ("Ortega", "Rafael", "surgery");
+
+INSERT INTO vet (lastname, firstname, speciality)
+VALUES ("Stevens", "Henry","radiology");
+
+INSERT INTO vet (lastname, firstname, speciality)
+VALUES ("Jenkins", "Sharon","none");
