68403159552dc233502a8b0e4693b3890d746e5c
Maxime Hempte <maximehempte@gmail.com>
2018-11-18 22:27:19 +0100

    Add HomePage

--- petclinic-web/web/src/Route.js
+++ petclinic-web/web/src/Route.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
+import HomePage from './components/Home/HomePage'
 import Vets from './components/Vets/Vets';
 
 export default () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Vets} />
+            <Route exact path="/" component={HomePage} />
             <Route path="/vets" component={Vets} />
         </Switch>
     </BrowserRouter>
