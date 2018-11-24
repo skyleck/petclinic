0f37c42adfb2e08af3213d2f61eb596220a20d2b
Maxime Hempte <maximehempte@gmail.com>
2018-11-24 12:15:41 +0100

    Add buttons updateOwner,deleteOwner and addPet

--- petclinic-web/web/src/components/Owner/Owner.js
+++ petclinic-web/web/src/components/Owner/Owner.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Pet from '../Pet/Pet'
+import { Link,NavLink } from 'react-router-dom';
 
-const Owner = ({lastname,firstname,address,city,telephone,pets,i}) => {
+const Owner = ({id,lastname,firstname,address,city,telephone,pets,i}) => {
 
     const petsHTML = pets.map( pet =>
         <Pet key={pet.name}{...pet}/>
@@ -19,6 +20,15 @@ const Owner = ({lastname,firstname,address,city,telephone,pets,i}) => {
                 <div>
                     <div className="title">City: </div> {city}
                     <div className="title">Telephone: </div> {telephone}
+                </div>
+                <div className="buttonContainer">
+                    <div className="buttonsOwner">
+                        <NavLink  exact to="/updateOwner">
+                            <div className="updateButton"></div>
+                        </NavLink>
+                        <div className="deleteButton">X</div>
+                    </div>
+                    <div className="addButton">+</div>
                 </div>
             </div>
             <div className="pets">
